76fdc17767d23a4ed07b97ffe53a4d238ce26533
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-04-25 14:26:19 -0700

    Taking third derivatives and defining rate of change...

--- main/main.ino
+++ main/main.ino
@@ -1,22 +1,25 @@
 #include <Servo.h>
 
-#define S1_PIN 7
-#define FULL_CYCLE 802.0
+#define S1_PIN A5
+#define FULL_CYCLE 850.0
+#define BEGIN_SPEED 15
 
 Servo s1;
 int angle, value;
 
 void setup() {
   Serial.begin(57600);
+  s1.attach(S1_PIN);
+  s1.writeMicroseconds(1500);
 }
 
 void loop() {
   if (Serial.available()) {
     angle = Serial.parseInt();
     value = floor(FULL_CYCLE * angle / 360.0);
-    s1.attach(S1_PIN);
-    s1.write(0);
+    //if (value > )
+    s1.writeMicroseconds(1000);
     delay(value);
-    s1.detach();
+    s1.writeMicroseconds(1500);
   }
 }

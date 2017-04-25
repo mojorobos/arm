0c3a12df37a1eb86111a3406a328b6600773c234
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2017-04-24 18:05:02 -0700

    Trying to calibrate the sensor

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,22 @@
-#include <Servo.h> 
+#include <Servo.h>
 
-#define S1_PIN A5
+#define S1_PIN 7
+#define FULL_CYCLE 802.0
 
 Servo s1;
-int a, b;
+int angle, value;
 
 void setup() {
-  s1.attach(S1_PIN);
-  a = 0;
-  b = -1;
-  s1.write(0);
+  Serial.begin(57600);
 }
 
 void loop() {
-  delay(10000);
-  b *= -1;
-  if (b > 0) {
-    s1.write (10);
-  } else {
-    s1.write(-10);
+  if (Serial.available()) {
+    angle = Serial.parseInt();
+    value = floor(FULL_CYCLE * angle / 360.0);
+    s1.attach(S1_PIN);
+    s1.write(0);
+    delay(value);
+    s1.detach();
   }
 }
